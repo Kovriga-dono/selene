6c5ec6d1585f6d0fcb355b558c1a315cdef2e268
kovriga <kvadrigahentai11@gmail.com>
2024-09-08 23:25:52 +0300

    updated conftest.py, added parametrize

--- conftest.py
+++ conftest.py
@@ -2,15 +2,25 @@ from selene.support.shared import browser
 import pytest
 
 
+def pytest_addoption(parser):
+    parser.addoption("--remote", action="store_true", default=False, help="Запустить браузер удаленно")
+    parser.addoption("--browser", action="store", default="chrome", help="Выбор браузера для запуска тестов")
+
+
+# def pytest_addoption(parser):
+#     parser.addoption("--remote", default=False, action='store_true',
+#                      help="Передаем - если надо запустить браузер удаленно")
+
+
 @pytest.fixture()
 def browser_c():
-    browser.config.browser_name = 'chrome'
+    browser.config.driver_name = 'chrome'
     browser.config.window_width = 1024
     browser.config.window_height = 768
 
 
 @pytest.fixture()
 def browser_f():
-    browser.browser_name = 'firefox'
+    browser.config.driver_name = 'firefox'
     browser.config.window_width = 1024
     browser.config.window_height = 768
--- pages/main_page.py
+++ pages/main_page.py
@@ -2,42 +2,41 @@ from selene.support.shared.jquery_style import s
 from selene.support.shared import browser
 from selene import query
 import re
-import models.locators
-import models.data
+from models import locators, data
 import allure
 
 
 # заполняем контактные данные
 def fill_contact_name():
     with allure.step('filling name'):
-        s(models.locators.firstname_selector).set(models.data.contact.first_name)
+        s(locators.firstname_selector).set(data.contact.first_name)
 
 
 def fill_contact_lastname():
     with allure.step('filling lastname'):
-        s(models.locators.lastname_selector).set(models.data.contact.last_name)
+        s(locators.lastname_selector).set(data.contact.last_name)
 
 
 def filling_category():
     with allure.step('filling category'):
-        s(models.locators.category_selector).click()
-        s(models.locators.category_selector_next).click()
+        s(locators.category_selector).click()
+        s(locators.category_selector_next).click()
 
 
 def filling_birthday():
     with allure.step('filling birthday'):
-        s(models.locators.birthday_selector).set(models.data.contact.birthday)
-        s(models.locators.firstname_selector).click()
+        s(locators.birthday_selector).set(data.contact.birthday)
+        s(locators.firstname_selector).click()
 
 
 def filling_addres():
     with allure.step('filling addres'):
         # берем оригинальное значение счетчика
-        c = re.search(r'\d+$', s(models.locators.counter_selector).get(query.text)).group()
-        s(models.locators.addres_selector).set(models.data.contact.address)
-        s(models.locators.button_selector).click()
+        c = re.search(r'\d+$', s(locators.counter_selector).get(query.text)).group()
+        s(locators.addres_selector).set(data.contact.address)
+        s(locators.button_selector).click()
         # берём счетчик после создания контакта
-        b = re.search(r'\d+$', s(models.locators.counter_selector).get(query.text)).group()
+        b = re.search(r'\d+$', s(locators.counter_selector).get(query.text)).group()
     # сравниваем счетчик до и после создания контакта
     assert int(b) == int(c) + 1
 
